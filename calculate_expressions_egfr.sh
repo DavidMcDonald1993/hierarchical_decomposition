16664c4b11b086f915817e793dbfc110becb934b
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-11-06 13:35:53 +0000

    python version fix

--- calculate_expressions_egfr.sh
+++ calculate_expressions_egfr.sh
@@ -14,4 +14,12 @@ args=$(echo "--edgelist datasets/EGFR_full/edgelist_with_genes.tsv
 --cancer_mutation erbb11 
 --output_genes elk1 creb ap1 cmyc p70s6_2 hsp27 pro_apoptotic")
 
+module purge
+module load bluebear
+module load bear-apps/2019a
+module load Python/3.7.2-GCCcore-8.2.0
+
+pip install --user numpy networkx pandas
+pip install --user PyBoolNet-2.2.6_linux64.tar.gz 
+
 python src/calculate_expressions.py $args -i ${SLURM_ARRAY_TASK_ID}
