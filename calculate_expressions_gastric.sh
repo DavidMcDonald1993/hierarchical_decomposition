86b1460bc6cc444536097f9fe50bc334e4525621
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-11-06 17:16:19 +0000

    chunk approach

--- calculate_expressions_gastric.sh
+++ calculate_expressions_gastric.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=expressionsGastric
 #SBATCH --output=expressionsGastric_%A_%a.out
 #SBATCH --error=expressionsGastric_%A_%a.err
-#SBATCH --array=0-2145
+#SBATCH --array=0-215
 #SBATCH --time=05:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=5G
